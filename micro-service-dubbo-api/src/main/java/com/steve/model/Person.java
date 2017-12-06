99520b948d4243869ea0da3d1f9baf7010a20c21
stevesdm <stevejobson@163.com>
2017-12-06 22:31:32 +0800

    delete useless config and add lombok

--- micro-service-dubbo-api/src/main/java/com/steve/model/Person.java
+++ micro-service-dubbo-api/src/main/java/com/steve/model/Person.java
@@ -1,10 +1,18 @@
 package com.steve.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 /**
  * Created by SteveJobson on 2017/7/13.
  */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -13,36 +21,4 @@ public class Person implements Serializable {
     Integer age;
     String address;
 
-    public Person() {
-    }
-
-    public Person(String name, Integer age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
