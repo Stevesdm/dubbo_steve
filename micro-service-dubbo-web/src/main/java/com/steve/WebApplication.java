fa2186630980433cc69092ec3ca65efa71a48279
stevesdm <stevejobson@163.com>
2017-12-27 16:55:34 +0800

    add module web and spring security

---
+++ micro-service-dubbo-web/src/main/java/com/steve/WebApplication.java
@@ -0,0 +1,18 @@
+package com.steve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: stevejobson
+ * @CreateDate: 2017/12/27 下午3:15
+ */
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
---
+++ micro-service-dubbo-web/src/main/java/com/steve/web/DemoController.java
@@ -0,0 +1,32 @@
+package com.steve.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Description:
+ * @Author: stevejobson
+ * @CreateDate: 2017/12/27 下午3:16
+ */
+@Controller
+public class DemoController {
+
+
+    @RequestMapping(value = {"/","/home"})
+    public ModelAndView home(){
+        return new ModelAndView("home");
+    }
+
+
+    @RequestMapping(value = "/hello")
+    public ModelAndView hello(){
+        return new ModelAndView("hello");
+    }
+
+    @RequestMapping(value = "/login")
+    public ModelAndView login(){
+        return new ModelAndView("login");
+    }
+
+}
