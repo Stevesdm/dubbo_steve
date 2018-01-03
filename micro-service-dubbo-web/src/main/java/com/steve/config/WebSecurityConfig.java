11dfe917fcaa7a51b4305ace8e21275082bccd3e
stevesdm <stevejobson@163.com>
2018-01-03 17:24:32 +0800

    基于方法的权限控制

--- micro-service-dubbo-web/src/main/java/com/steve/config/WebSecurityConfig.java
+++ micro-service-dubbo-web/src/main/java/com/steve/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,8 +21,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //通过注解开启基于方法的权限控制
 
     @Bean
     public UserDetailsService getSteveUserService() {
